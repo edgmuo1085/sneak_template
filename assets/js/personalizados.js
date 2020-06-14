b807dd4e1700c33a909174618a4ce0c15834ec79
edgmuo1085 <edgmuo1085@gmail.com>
2020-06-14 00:49:06 -0500

    Creación de plantilla en bootstrap4, con html5, JS para una empresa bogotana

---
+++ assets/js/personalizados.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	//  Activate the Tooltips
+	$('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
+
+	// Activate Popovers
+	$('[data-toggle="popover"]').popover();
+});
